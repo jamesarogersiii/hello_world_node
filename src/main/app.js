ded152ffd10607b63fcadb008c69505ab37ce7a4
james <james.a.rogers.iii@gmail.com>
2021-02-20 12:10:58 -0500

    Fixed response format

--- src/main/app.js
+++ src/main/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const greeter = require('./greeter')
 const app = express();
 
-app.get('/', function (req, res) { res.send(greeter.greeting()) });
+app.get('/', function (req, res) { res.json(greeter.greeting()) });
 
 app.listen(8080, function () { console.log("Service running https://localhost:8080") });
 
