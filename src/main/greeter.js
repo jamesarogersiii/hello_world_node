8ceaae0d4a77ba955285a70abd3b514f354d75a7
james <james.a.rogers.iii@gmail.com>
2021-02-21 13:27:21 -0500

    Updated greeting translations

--- src/main/greeter.js
+++ src/main/greeter.js
@@ -1,25 +1,116 @@
 module.exports = {
   greeting(){ 
     return [
-      "Hello World",
-      "Hola Mundo",
-      "Ciao mondo",
-      "Bonjour le monde",
-      "你好，世界",
-      "Hallo Welt",
-      "Привет, мир",
-      "こんにちは世界",
-      "Γειά σου Κόσμε",
-      "שלום עולם",
-      "สวัสดีชาวโลก",
-      "Ahoj světe",
-      "Tere, Maailm",
-      "Labas pasauli",
-      "Hei Verden",
-      "హలో వరల్డ్",
-      "Hej världen",
-      "Witaj świecie",
-      "Kamusta Mundo"
+      {
+        "language": "Czech",
+        "symbol": "cs",
+        "text": "Ahoj světe"
+      },
+      {
+        "language": "Estonian",
+        "symbol": "et",
+        "text": "Tere, Maailm"
+      },
+      {
+        "language": "Lithuanian",
+        "symbol": "lt",
+        "text": "Labas pasauli"
+      },
+      {
+        "language": "Norwegian",
+        "symbol": "no",
+        "text": "Hei Verden"
+      },
+      {
+        "language": "Telugu",
+        "symbol": "te",
+        "text": "హలో వరల్డ్"
+      },
+      {
+        "language": "Swedish",
+        "symbol": "sv",
+        "text": "Hej världen"
+      },
+      {
+        "language": "Polish",
+        "symbol": "pl",
+        "text": "Witaj świecie"
+      },
+      {
+        "language": "Filipino",
+        "symbol": "tl",
+        "text": "Kamusta Mundo"
+      },
+      {
+        "language": "Danish",
+        "symbol": "da",
+        "text": "Hej Verden"
+      },
+      {
+        "language": "Portuguese",
+        "symbol": "pt",
+        "text": "Olá Mundo"
+      },
+      {
+        "language": "German",
+        "symbol": "de",
+        "text": "Hallo Welt"
+      },
+      {
+        "language": "Italian",
+        "symbol": "it",
+        "text": "Ciao mondo"
+      },
+      {
+        "language": "Russian",
+        "symbol": "ru",
+        "text": "Привет, мир"
+      },
+      {
+        "language": "Spanish",
+        "symbol": "es",
+        "text": "Hola Mundo"
+      },
+      {
+        "language": "Japanese",
+        "symbol": "ja",
+        "text": "こんにちは世界"
+      },
+      {
+        "language": "Greek",
+        "symbol": "el",
+        "text": "Γειά σου Κόσμε"
+      },
+      {
+        "language": "Chinese Simplified",
+        "symbol": "zh-cn",
+        "text": "你好，世界"
+      },
+      {
+        "language": "Thai",
+        "symbol": "th",
+        "text": "สวัสดีชาวโลก"
+      },
+      {
+        "language": "Korean",
+        "symbol": "ko",
+        "text": "Hello World"
+      },
+      {
+        "language": "Finnish",
+        "symbol": "fi",
+        "text": "Hei maailma"
+      },
+      {
+        "language": "Hebrew",
+        "symbol": "iw",
+        "text": "שלום עולם"
+      },
+      {
+        "language": "French",
+        "symbol": "fr",
+        "text": "Bonjour le monde"
+      }
     ]; 
   }
 }
