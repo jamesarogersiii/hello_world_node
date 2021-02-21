c81edb52e91751155031d408ffb2e12e23026683
james <james.a.rogers.iii@gmail.com>
2021-02-21 13:20:53 -0500

    Added greeting translations

--- src/main/greeter.js
+++ src/main/greeter.js
@@ -1,3 +1,25 @@
 module.exports = {
-  greeting(){ return { greeting: "Hello World" }; }
+  greeting(){ 
+    return [
+      "Hello World",
+      "Hola Mundo",
+      "Ciao mondo",
+      "Bonjour le monde",
+      "你好，世界",
+      "Hallo Welt",
+      "Привет, мир",
+      "こんにちは世界",
+      "Γειά σου Κόσμε",
+      "שלום עולם",
+      "สวัสดีชาวโลก",
+      "Ahoj světe",
+      "Tere, Maailm",
+      "Labas pasauli",
+      "Hei Verden",
+      "హలో వరల్డ్",
+      "Hej världen",
+      "Witaj świecie",
+      "Kamusta Mundo"
+    ]; 
+  }
 }
--- src/test/greeter.js
+++ src/test/greeter.js
@@ -2,6 +2,28 @@ const greeter = require('../main/greeter.js');
  
 describe('greeting', () => {
   it('is Hello World', () => {
-    expect(greeter.greeting()).toEqual({"greeting":"Hello World"});
+    expect(greeter.greeting()).toEqual(
+        [
+            "Hello World",
+            "Hola Mundo",
+            "Ciao mondo",
+            "Bonjour le monde",
+            "你好，世界",
+            "Hallo Welt",
+            "Привет, мир",
+            "こんにちは世界",
+            "Γειά σου Κόσμε",
+            "שלום עולם",
+            "สวัสดีชาวโลก",
+            "Ahoj světe",
+            "Tere, Maailm",
+            "Labas pasauli",
+            "Hei Verden",
+            "హలో వరల్డ్",
+            "Hej världen",
+            "Witaj świecie",
+            "Kamusta Mundo"
+          ]
+    );
   });
 });
