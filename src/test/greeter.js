d32ab0d67eed01a0d2544cccc7ef5ac368f62488
james <james.a.rogers.iii@gmail.com>
2021-04-17 19:16:39 -0400

    Updated test

--- src/test/greeter.js
+++ src/test/greeter.js
@@ -3,118 +3,118 @@ const greeter = require('../main/greeter.js');
 describe('greeting', () => {
   it('is Hello World', () => {
     expect(greeter.greeting()).toEqual(
-        [
-            {
-              "language": "Czech",
-              "symbol": "cs",
-              "text": "Ahoj světe"
-            },
-            {
-              "language": "Estonian",
-              "symbol": "et",
-              "text": "Tere, Maailm"
-            },
-            {
-              "language": "Lithuanian",
-              "symbol": "lt",
-              "text": "Labas pasauli"
-            },
-            {
-              "language": "Norwegian",
-              "symbol": "no",
-              "text": "Hei Verden"
-            },
-            {
-              "language": "Telugu",
-              "symbol": "te",
-              "text": "హలో వరల్డ్"
-            },
-            {
-              "language": "Swedish",
-              "symbol": "sv",
-              "text": "Hej världen"
-            },
-            {
-              "language": "Polish",
-              "symbol": "pl",
-              "text": "Witaj świecie"
-            },
-            {
-              "language": "Filipino",
-              "symbol": "tl",
-              "text": "Kamusta Mundo"
-            },
-            {
-              "language": "Danish",
-              "symbol": "da",
-              "text": "Hej Verden"
-            },
-            {
-              "language": "Portuguese",
-              "symbol": "pt",
-              "text": "Olá Mundo"
-            },
-            {
-              "language": "German",
-              "symbol": "de",
-              "text": "Hallo Welt"
-            },
-            {
-              "language": "Italian",
-              "symbol": "it",
-              "text": "Ciao mondo"
-            },
-            {
-              "language": "Russian",
-              "symbol": "ru",
-              "text": "Привет, мир"
-            },
-            {
-              "language": "Spanish",
-              "symbol": "es",
-              "text": "Hola Mundo"
-            },
-            {
-              "language": "Japanese",
-              "symbol": "ja",
-              "text": "こんにちは世界"
-            },
-            {
-              "language": "Greek",
-              "symbol": "el",
-              "text": "Γειά σου Κόσμε"
-            },
-            {
-              "language": "Chinese Simplified",
-              "symbol": "zh-cn",
-              "text": "你好，世界"
-            },
-            {
-              "language": "Thai",
-              "symbol": "th",
-              "text": "สวัสดีชาวโลก"
-            },
-            {
-              "language": "Korean",
-              "symbol": "ko",
-              "text": "Hello World"
-            },
-            {
-              "language": "Finnish",
-              "symbol": "fi",
-              "text": "Hei maailma"
-            },
-            {
-              "language": "Hebrew",
-              "symbol": "iw",
-              "text": "שלום עולם"
-            },
-            {
-              "language": "French",
-              "symbol": "fr",
-              "text": "Bonjour le monde"
-            }
-          ]
+      [
+        {
+          "language": "English",
+          "symbol": "en",
+          "text": "Hello World"
+        },
+        {
+          "language": "Czech",
+          "symbol": "cs",
+          "text": "Ahoj světe"
+        },
+        {
+          "language": "Estonian",
+          "symbol": "et",
+          "text": "Tere, Maailm"
+        },
+        {
+          "language": "Lithuanian",
+          "symbol": "lt",
+          "text": "Labas pasauli"
+        },
+        {
+          "language": "Norwegian",
+          "symbol": "no",
+          "text": "Hei Verden"
+        },
+        {
+          "language": "Telugu",
+          "symbol": "te",
+          "text": "హలో వరల్డ్"
+        },
+        {
+          "language": "Swedish",
+          "symbol": "sv",
+          "text": "Hej världen"
+        },
+        {
+          "language": "Polish",
+          "symbol": "pl",
+          "text": "Witaj świecie"
+        },
+        {
+          "language": "Filipino",
+          "symbol": "tl",
+          "text": "Kamusta Mundo"
+        },
+        {
+          "language": "Danish",
+          "symbol": "da",
+          "text": "Hej Verden"
+        },
+        {
+          "language": "Portuguese",
+          "symbol": "pt",
+          "text": "Olá Mundo"
+        },
+        {
+          "language": "German",
+          "symbol": "de",
+          "text": "Hallo Welt"
+        },
+        {
+          "language": "Italian",
+          "symbol": "it",
+          "text": "Ciao mondo"
+        },
+        {
+          "language": "Russian",
+          "symbol": "ru",
+          "text": "Привет, мир"
+        },
+        {
+          "language": "Spanish",
+          "symbol": "es",
+          "text": "Hola Mundo"
+        },
+        {
+          "language": "Japanese",
+          "symbol": "ja",
+          "text": "こんにちは世界"
+        },
+        {
+          "language": "Greek",
+          "symbol": "el",
+          "text": "Γειά σου Κόσμε"
+        },
+        {
+          "language": "Chinese Simplified",
+          "symbol": "zh-cn",
+          "text": "你好，世界"
+        },
+        {
+          "language": "Thai",
+          "symbol": "th",
+          "text": "สวัสดีชาวโลก"
+        },
+        {
+          "language": "Finnish",
+          "symbol": "fi",
+          "text": "Hei maailma"
+        },
+        {
+          "language": "Hebrew",
+          "symbol": "iw",
+          "text": "שלום עולם"
+        },
+        {
+          "language": "French",
+          "symbol": "fr",
+          "text": "Bonjour le monde"
+        }
+      ]
     );
   });
 });
